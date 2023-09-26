454a650f1058445d56c59069770373f754772c8d
Juan José Arreola Gonzalez <juanjosearreola99@gmail.com>
2023-09-25 20:30:06 -0600

    Update 3 e import de Resources

--- AppSuperHero/app/src/main/java/net/juanaghuachi/appsuperhero/HeroesScreen.kt
+++ AppSuperHero/app/src/main/java/net/juanaghuachi/appsuperhero/HeroesScreen.kt
@@ -1,6 +1,6 @@
 package net.juanaghuachi.appsuperhero
 
-import android.inputmethodservice.Keyboard.Row
+import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.MutableTransitionState
@@ -9,28 +9,43 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.juanaghuachi.appsuperhero.model.Hero
+import net.juanaghuachi.appsuperhero.model.HeroesRepository
+import net.juanaghuachi.appsuperhero.ui.theme.SuperheroesTheme
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HeroesList(
-    heores: List<Hero>,
+    heroes: List<Hero>,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
@@ -41,15 +56,15 @@ fun HeroesList(
     }
 
     AnimatedVisibility(
-        visible = visibleState,
+        visibleState = visibleState,
         enter = fadeIn(
             animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy)
         ),
         exit = fadeOut(),
         modifier = modifier
     ) {
-        LazyColumn(content = contentPadding) {
-            itemsIndexed(heores) { index, hero ->
+        LazyColumn(contentPadding = contentPadding) {
+            itemsIndexed(heroes) { index, hero ->
                 HeroListItem(hero = hero,
                     modifier = modifier
                         .padding(horizontal = 16.dp, vertical = 8.dp)
@@ -85,13 +100,51 @@ fun HeroListItem(
         ) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(
-                    text = stringResource(id = hero.descriptionRes),
+                    text = stringResource(id = hero.nameRes),
                     style = MaterialTheme.typography.displaySmall
                 )
-
+                Text(
+                    text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
-
+            Spacer(Modifier.width(16.dp))
+            Box(modifier = Modifier
+                .size(72.dp)
+                .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
         }
+    }
+}
 
+@Preview("Light Theme")
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HeroPreview(){
+    val hero = Hero(
+        R.string.hero1,
+        R.string.description1,
+        R.drawable.android_superhero1
+    )
+    SuperheroesTheme {
+        HeroesList(heroes = hero)
+    }
+}
+
+@Preview("Lista de Heroes")
+@Composable
+fun HeroesPreview(){
+    SuperheroesTheme(darkTheme = false) {
+        Surface (
+            color = MaterialTheme.colorScheme.background
+        ){
+            HeroesList(heroes = HeroesRepository.heroes)
+        }
     }
 }
