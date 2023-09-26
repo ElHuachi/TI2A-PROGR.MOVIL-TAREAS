2228b05562656ceccd5aedd9842472fb95ab2c7f
Juan José Arreola Gonzalez <juanjosearreola99@gmail.com>
2023-09-25 20:14:36 -0600

    Update 2

--- AppSuperHero/app/src/main/java/net/juanaghuachi/appsuperhero/HeroesScreen.kt
+++ AppSuperHero/app/src/main/java/net/juanaghuachi/appsuperhero/HeroesScreen.kt
@@ -18,9 +18,12 @@ import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import net.juanaghuachi.appsuperhero.model.Hero
 
@@ -81,6 +84,10 @@ fun HeroListItem(
                 .sizeIn(minHeight = 72.dp)
         ) {
             Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.displaySmall
+                )
 
             }
 
